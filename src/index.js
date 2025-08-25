c668df1a6d7d4c99f5e3914889149fe46ed2dd19
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-08-25 13:38:43 +0200

    feat: function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const pares = {};
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    const [open, close] = bracketsConfig[i];
+    pares[open] = close;
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    if (stack.length === 0) {
+      stack.push(str[i]);
+    } else if (pares[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  if (stack.length > 0) return false;
+  return true;
 };
